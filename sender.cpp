f4577a482f3fcd5e4d05694276f77ba834ed3081
Liam Finn <lfinn2@jhu.edu>
2023-11-29 11:48:37 -0500

    working on sender

--- sender.cpp
+++ sender.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // TODO: send slogin message
+  // send slogin message
   Message slogin_msg(TAG_SLOGIN, username);
   Message login_response;
   conn.send(slogin_msg);
@@ -40,8 +40,63 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
+  Message response;
+  std::string user_input;
+
+  // loop reading commands from user, sending messages to server as appropriate
+  while (true) {
+    std::getline(std::cin, user_input);
+    //Commands
+    if(user_input.at(0) == '/'){
+      if(user_input == "/quit"){  //quit
+        response.tag = TAG_QUIT;
+        response.data = " ";
+        conn.send(response);
+        conn.receive(response); //wait for server response before quitting
+        if (response.tag == TAG_ERR) { 
+          fprintf(stderr, response.data.c_str());
+        }
+        return 0;
+
+      } else if(user_input == "/leave") { //leave
+        response.tag = TAG_LEAVE;
+        response.data = " ";
+        conn.send(response);
+
+      } else if(user_input == "/join"){   //join
+        response.tag = TAG_JOIN;
+        response.data = user_input.substr(6); //room name after "/join "
+        conn.send(response);
+      }
+
+      else {
+        fprintf(stderr, "Command not recognized\n");
+      }
+
+    //otherwise, trying to send message
+    } else { 
+      if(user_input.length() > response.MAX_LEN) {
+        //TODO: WHAT HAPPENS IF INPUT MESSAGE IS TOO LONG
+      } else{
+        response.tag = TAG_SENDALL;
+        response.data = user_input;
+        conn.send(response);
+      }
+    }
+
+    conn.receive(response); 
+    if (response.tag == TAG_ERR) { 
+      fprintf(stderr, response.data.c_str());
+    }
+
+  }
+   //while loop
+  //wait for stdin input (Commands start with the / character and may be one of the following, reject rest to stderr)
+  //if quit, break loop
+    // the sender must wait for a reply from the server before exiting with exit code 0.
+  //if join, join room
+  //if leave, leave the room
+  //if message, send message in room
 
   return 0;
 }
